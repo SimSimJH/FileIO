dca8e166b6b9a034e3d3f8b9e1595d720e24913d
qkskq <qkskq@DESKTOP-7GVLUUM>
2020-06-03 02:59:45 +0900

    Modify UsingBufferedReader.java

--- src/main/java/edu/handong/csee/java/example/fileread/benchmark/UsingBufferedReader.java
+++ src/main/java/edu/handong/csee/java/example/fileread/benchmark/UsingBufferedReader.java
@@ -7,8 +7,30 @@ import java.io.*;
  */
 public class UsingBufferedReader {
     public static void main(String[] args) throws IOException {
+    	
+    	//-------------- Test reading 1 MB file. --------------------
+    	 StopWatch.start();
 
+         BufferedReader inputStream= new BufferedReader(new FileReader(args[0]));
+         while (inputStream.read()!=-1){}
 
+         long duration = StopWatch.stop();
+         System.out.println(duration + " milsec");
+         
+         inputStream.close();
+         
+       //-------------- Test reading 10 MB file. --------------------
+
+         StopWatch.start();
+
+         BufferedReader inputStream2= new BufferedReader(new FileReader(args[1]));
+         while (inputStream2.read()!=-1){}
+
+         long duration2 = StopWatch.stop();
+         System.out.println(duration2 + " milsec");
+
+         inputStream2.close();
+/*
         //-------------- Test reading 1 MB file. --------------------
 
         StopWatch.start();
@@ -33,7 +55,7 @@ public class UsingBufferedReader {
         System.out.println(duration2 + " milsec");
 
         inputStream2.close();
-
+*/
         /*
         //-------------- Test reading 100 MB file. --------------------
 
